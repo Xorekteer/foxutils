140000099dbe38a345be88b23e6a2ea953b1ca55
Xorekteer <>
2020-10-12 14:33:56 +0200

    Added recursive option to foxmangle.

--- webtools/foxmangle.py
+++ webtools/foxmangle.py
@@ -1,25 +1,33 @@
 if False:   # VS code linting trick
     import foxlaff
     import foxre
+    import foxcast
 
 # Real imports
 import x_foxport    # appends foxutils root to path
                     # doesn't activate VS code linting
 import foxutils.foxlaff as foxlaff
 import foxutils.foxre   as foxre
+import foxutils.foxcast as foxcast
 
 import os
 import re
 """
-Mangles all html or css (or whatever file type there is a function for) in the current folder.
-Improvement suggestion: recurively handle all files in subfolders.
+Improvement suggestion: refactor.
 """
 
-def mangle_all_html():
+
+def mangle_all_html(recursive=False):
     """
     Removes all lines that are empty or contain comments from all htmls within the current folder.
     """
-    filenames_here = foxlaff.load_all_from_folder( os.getcwd() )
+    if recursive:
+        subfolders     = foxlaff.list_subfolders_recursively(os.getcwd(), include_root=True)
+        filenames_here = [foxlaff.load_all_from_folder( folder ) for folder in subfolders]
+        filenames_here = foxcast.flatten_iterable(filenames_here)
+    else:
+        filenames_here = foxlaff.load_all_from_folder( os.getcwd() )
+    
     html_filenames = foxre.relist("\.html", filenames_here)
     for filename in html_filenames:
         result_lines  = list()
@@ -36,10 +44,17 @@ def mangle_all_html():
             current_file.writelines(result_lines)
             
             
-def mangle_all_snek():
+def mangle_all_snek(recursive=False):
     """
     Removes all lines containing only whitespace or # comments from all python files within current folder.
     """
+    if recursive:
+        subfolders     = foxlaff.list_subfolders_recursively(os.getcwd(), include_root=True)
+        filenames_here = [foxlaff.load_all_from_folder( folder ) for folder in subfolders]
+        filenames_here = foxcast.flatten_iterable(filenames_here)
+    else:
+        filenames_here = foxlaff.load_all_from_folder( os.getcwd() )
+    
     filenames_here = foxlaff.load_all_from_folder( os.getcwd() )
     html_filenames = foxre.relist("\.py", filenames_here)
     for filename in html_filenames:
@@ -57,5 +72,5 @@ def mangle_all_snek():
             current_file.writelines(result_lines)
             
 if __name__ == "__main__":
-    mangle_all_html()
-    mangle_all_snek()
+    mangle_all_html(recursive=True)
+    mangle_all_snek(recursive=True)
